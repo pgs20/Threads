228a212fe242cdd8d297b6c7fbf666036ce9b85d
Gosha <gosha_petrov20@rambler.ru>
2024-03-05 21:57:34 +0300

    Поработал с Callable, ExecutorService и Future

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        ThreadGroup group = new ThreadGroup("group");
-
-        System.out.println("Создаю потоки...");
-        Thread thread1 = new MyThread(group, "Поток-1");
-        Thread thread2 = new MyThread(group, "Поток-2");
-        Thread thread3 = new MyThread(group, "Поток-3");
-        Thread thread4 = new MyThread(group, "Поток-4");
-
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-
-        Thread.sleep(10000);
-        System.out.println("Завершаю все потоки.");
-        group.interrupt();
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        List<MyThread> callable = new ArrayList<>(Arrays.asList(new MyThread(3), new MyThread(2)));
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        List<Future<Integer>> futureList = executor.invokeAll(callable);
+        System.out.println("Количество строк " + futureList.stream().mapToInt(i -> {
+            try {
+                return i.get();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }).sum());
+        executor.shutdown();
     }
 }
--- src/main/java/MyThread.java
+++ src/main/java/MyThread.java
@@ -1,17 +1,16 @@
-public class MyThread extends Thread {
-    public MyThread(ThreadGroup threadGroup, String name) {
-        super(threadGroup, name);
-    }
+import java.util.concurrent.Callable;
 
+public class MyThread implements Callable<Integer> {
+    private int n;
+
+    public MyThread(int n) {
+        this.n = n;
+    }
     @Override
-    public void run() {
-        try {
-            while (!isInterrupted()) {
-                Thread.sleep(2500);
-                System.out.println("Я " + Thread.currentThread().getName() + ". Всем привет!");
-            }
-        } catch (InterruptedException e) {
-            e.getMessage();
+    public Integer call() throws Exception {
+        for (int i = 0; i < n; ++i) {
+            System.out.println("Я " + Thread.currentThread().getName());
         }
+        return n;
     }
 }
